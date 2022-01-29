657beb44f1fa251e6e01d3152662efeb7c43c0e3
Gisbelc <gisbel.contreras.20@gmail.com>
2022-01-29 00:48:51 -0300

    Se agrega el Proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,33 @@
-const cipher = {
-  // ...
-};
+const cipher =  {
+    //CIFRAR 
+    encode: (mensaje, desplazamiento) => { 
+      let resultado = "";
+      for (let i = 0; i < mensaje.length; i++) {
+        let ascii = mensaje[i].charCodeAt();
+        if (ascii >= 65 && ascii <= 90) {        
+          let newAscii = (((ascii - 65 + parseInt(desplazamiento)) % 26) + 65);
+          resultado += String.fromCharCode(newAscii);
+        } else {
+          resultado += mensaje[i];
+        }
+      }
+      return resultado;
+    },
 
+    decode: (mensaje2, desplazamiento) => {
+      let resultado = " ";
+      for (let i = 0; i < mensaje2.length; i++) {
+        let ascii = mensaje2[i].charCodeAt();
+        if (ascii >= 65 && ascii <= 90) {
+          let newAscii = (((ascii + 65 - parseInt(desplazamiento)) % 26) + 65);
+          resultado += String.fromCharCode(newAscii);
+        } else {
+          resultado += mensaje2[i];
+        } 
+      }
+      return resultado;
+    },
+
+      
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+    
+ //CIFRAR
 
+document.getElementById("cifrar").addEventListener("click", ()=>{
+    let mensaje = document.getElementById("mensaje").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("mensaje2").value = cipher.encode(mensaje.toUpperCase(), desplazamiento); 
+});
+
+//DESCIFRAR
+document.getElementById("descifrar").addEventListener("click", ()=>{ 
+    let mensaje2 = document.getElementById("mensaje2").value;
+    let desplazamiento = document.getElementById("desplazamiento").value;
+    document.getElementById("mensaje2").value = cipher.decode(mensaje2.toUpperCase(), desplazamiento);   
+});
 console.log(cipher);
+
