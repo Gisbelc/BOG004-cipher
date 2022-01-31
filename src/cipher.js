fa678a3a71d198902ea2a110aa01afa0a5bc1452
Gisbelc <gisbel.contreras.20@gmail.com>
2022-01-30 23:53:57 -0300

    readme

--- src/cipher.js
+++ src/cipher.js
@@ -6,7 +6,7 @@ const cipher =  {
         throw new TypeError();
       }
       let resultado = " "; //almacenara el caracter cifrado 
-      for (let i = 0; i < mensaje.length; i++) {  //recorre los caracteres 
+      for (let i = 0; i < mensaje.length; i++) {  //recorre los caracteres    
         let ascii = mensaje[i].charCodeAt(); //arroja el codigo ascii a mensaje  
         if (ascii >= 65 && ascii <= 90) {        
           let newAscii = (((ascii - 65 + parseInt(desplazamiento)) % 26) + 65); //si el ascii es mayor o igual a 65 o menor igual a 90 entonces 
@@ -18,11 +18,11 @@ const cipher =  {
       return resultado;
     },
      //DESCIFRAR
-    decode: (mensaje2, desplazamiento) => { 
+    decode: (mensaje2, desplazamiento) => {
 
         if (!mensaje2 && !desplazamiento) {
-        throw new TypeError();
-      }
+        throw new TypeError(); 
+        }    
       let resultado = " ";
       for (let i = 0; i < mensaje2.length; i++) {
         let ascii = mensaje2[i].charCodeAt(); 
