1fcfba6fb5c7f1c11bd60c004dce76e773bfa78c
Gisbelc <gisbel.contreras.20@gmail.com>
2022-02-01 14:35:09 -0300

    cambio html css

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,43 @@
 const cipher =  {     
     //CIFRAR 
-    encode: (mensaje, desplazamiento) => { 
+    encode: (desplazamiento, mensaje) => { 
 
-      if (!mensaje && !desplazamiento) {
+//Si alguno de los parametros se encuentran vacios debe arrojar TypeError 
+      if (!desplazamiento || !mensaje) {
         throw new TypeError();
       }
-      let resultado = " "; //almacenara el caracter cifrado 
+
+      let resultado = ""; //almacenara el caracter cifrado 
       for (let i = 0; i < mensaje.length; i++) {  //recorre los caracteres    
         let ascii = mensaje[i].charCodeAt(); //arroja el codigo ascii a mensaje  
         if (ascii >= 65 && ascii <= 90) {        
           let newAscii = (((ascii - 65 + parseInt(desplazamiento)) % 26) + 65); //si el ascii es mayor o igual a 65 o menor igual a 90 entonces 
           resultado += String.fromCharCode(newAscii); //arroja su pocision en letra
-        } else {
+        } else { //Si no se cumple la condicion devuelve mensaje con el valor agregado sin obtener el codigo ascii
           resultado += mensaje[i]; 
       }
     }
       return resultado;
     },
      //DESCIFRAR
-    decode: (mensaje2, desplazamiento) => {
-
-        if (!mensaje2 && !desplazamiento) {
+    decode: (desplazamiento, mensaje2) => {
+//Si alguno de los parametros se encuentran vacios debe arrojar TypeError 
+        if (!mensaje2 || !desplazamiento) {
         throw new TypeError(); 
         }    
-      let resultado = " ";
-      for (let i = 0; i < mensaje2.length; i++) {
-        let ascii = mensaje2[i].charCodeAt(); 
+
+      let resultado = "";  //almacenara el caracter descifrado
+      for (let i = 0; i < mensaje2.length; i++) { //recorre los caracteres  
+        let ascii = mensaje2[i].charCodeAt(); //arroja el codigo ascii a mensaje  
         if (ascii >= 65 && ascii <= 90) {
-          let newAscii = (((ascii + 65 - parseInt(desplazamiento)%26) % 26) + 65);
-          resultado += String.fromCharCode(newAscii);
-        } else {
+          let newAscii = (((ascii + 65 - parseInt(desplazamiento)%26) % 26) + 65); //si el ascii es mayor o igual a 65 o menor igual a 90 entonces 
+          resultado += String.fromCharCode(newAscii); //arroja su posicion en letra 
+        } else { //Si no se cumple la condicion devuelve mensaje con el valor agregado sin obtener el codigo ascii
           resultado += mensaje2[i];
         } 
       }
       return resultado;
-    },
+    }, 
       
 };
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import cipher from './cipher.js';
 
  //CIFRAR
-document.getElementById("cifrar").addEventListener("click", ()=>{
+document.getElementById("cifrar").addEventListener("click", () =>{
     let mensaje = document.getElementById("mensaje").value;
     let desplazamiento = document.getElementById("desplazamiento").value;
-    document.getElementById("mensaje2").value = cipher.encode(mensaje.toUpperCase(), desplazamiento); 
+    document.getElementById("mensaje2").value = cipher.encode(desplazamiento, mensaje.toUpperCase()); 
 });
 
 //DESCIFRAR
 document.getElementById("descifrar").addEventListener("click", ()=>{ 
-    let mensaje2 = document.getElementById("mensaje2").value;
+    let mensaje2 = document.getElementById("mensaje").value;
     let desplazamiento = document.getElementById("desplazamiento").value;
-    document.getElementById("mensaje2").value = cipher.decode(mensaje2.toUpperCase(), desplazamiento);   
+    
+    document.getElementById("mensaje2").value = cipher.decode(desplazamiento, mensaje2.toUpperCase());   
 });
 console.log(cipher);
 
